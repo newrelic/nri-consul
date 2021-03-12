9c81278d3f3210933548004022c72b56155f3e50
Stamatis Katsaounis <katsaouniss@gmail.com>
2021-03-12 09:26:13 +0200

    Delete win-definition file

--- build/nix/fix_archives.sh
+++ build/nix/fix_archives.sh
@@ -23,7 +23,6 @@ find dist -regex ".*_dirty\.tar.gz" | while read tarball_dirty; do
 
   echo "===> Move files inside ${tarball}"
   mv ${TARBALL_CONTENT_PATH}/nri-${INTEGRATION} "${TARBALL_CONTENT_PATH}/var/db/newrelic-infra/newrelic-integrations/bin/"
-  mv ${TARBALL_CONTENT_PATH}/${INTEGRATION}-definition.yml ${TARBALL_CONTENT_PATH}/var/db/newrelic-infra/newrelic-integrations/
   mv ${TARBALL_CONTENT_PATH}/${INTEGRATION}-config.yml.sample ${TARBALL_CONTENT_PATH}/etc/newrelic-infra/integrations.d/
 
   echo "===> Creating tarball ${TARBALL_CLEAN}"
